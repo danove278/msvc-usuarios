ad4eb2dc2060897856977f0f16ae2d528f2f57ec
danove <danove278@gmail.com>
2023-05-26 19:08:52 -0400

    Agregamos el import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+/* Populate tabla usuarios */
+
+INSERT INTO usuarios (nombre,  email, password) VALUES('Andrés',  'profesor@bolsadeideas.com', '2018-01-01');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Mr. John',  'john.doe@gmail.com', '2018-01-02');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Linus',  'linus.torvalds@gmail.com', '2018-01-03');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Rasmus',  'rasmus.lerdorf@gmail.com', '2018-01-04');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Erich',  'erich.gamma@gmail.com', '2018-02-01');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Richard',  'richard.helm@gmail.com', '2018-02-10');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Ralph',  'ralph.johnson@gmail.com', '2018-02-18');
+INSERT INTO usuarios (nombre,  email, password) VALUES('John',  'john.vlissides@gmail.com', '2018-02-28');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Dr. James',  'james.gosling@gmail.com', '2018-03-03');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Magma',  'magma.lee@gmail.com', '2018-03-04');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Tornado',  'tornado.roe@gmail.com', '2018-03-05');
+INSERT INTO usuarios (nombre,  email, password) VALUES('Jade',  'jane.doe@gmail.com', '2018-03-06');
